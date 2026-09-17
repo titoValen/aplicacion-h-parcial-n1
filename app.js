d6f23031fda376ca3580e794b9bc0ff6985ba00d
titoValen <valentin.tito@davinci.edu.ar>
2026-09-17 01:52:47 -0300

    feat: Agregar rutas para el manejo de asistentes en la API

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import eventRoutes from "./api/routes/event.routes.js";
+import attendeeRoutes from "./api/routes/attendee.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/events", eventRoutes);
+app.use("/api/attendees", attendeeRoutes);
 
 const startServer = async () => {
   try {
